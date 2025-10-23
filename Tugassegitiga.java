78609e893814b20c86c5e0c05998be4980d75c43
nayladisisitugas05-web <nayla.disisitugas05@gmail.com>
2025-10-23 12:38:35 +0700

    Create Tugassegitiga.java

---
+++ Tugassegitiga.java
@@ -0,0 +1,17 @@
+public class Tugassegitiga {
+
+    public static void main(String[] args) {
+        int a, b, c;
+        
+        
+        for (a=1; a<=5; a++){
+            for (b=a; b<=5; b++){
+                System.out.print(" ");
+            }
+            for (c=1; c<=(2*a-1); c++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
